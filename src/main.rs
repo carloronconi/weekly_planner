4373fe9b1d66faf134ce195fd50d13d9fd2d9980
carloronconi <carlo.ronconi@mail.polimi.it>
2023-10-29 17:53:18 +0100

    Refactor into separate procedures to improve code readability

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use regex::Regex;
 use chrono::{Days, NaiveDate};
 
@@ -12,7 +12,6 @@ const FILE_CONTENT_PATTERN: &str = r"^# Todos week [0-9]{8}\n"; // simplified fi
 const FILE_DELETION_PATTERN: &str = r"- \[x\]";
 
 fn main() {
-
     // parse command line arguments
     let args: Vec<String> = env::args().collect();
     let dir_path = match args.get(1) {
@@ -21,59 +20,72 @@ fn main() {
     };
 
     // explore the directory to find the most recent weekly plan
-    let latest_week_file_path = find_latest_weekly_plan(dir_path);
+    let latest_week_file_path = find_latest_weekly_plan(dir_path).expect("Couldn't find weekly plan file in directory");
     let latest_week_file_path = latest_week_file_path.as_path();
     println!("Found latest week plan in provided directory: {:?}", latest_week_file_path);
 
     // check if the most recent weekly plan has the correct content
-    let latest_week_file_content = fs::read_to_string(&latest_week_file_path)
-        .expect("Couldn't read the file!");
-    let regex = Regex::new(FILE_CONTENT_PATTERN).unwrap();
-    if !regex.is_match(&latest_week_file_content) { panic!("File content doesn't match the expected pattern {FILE_CONTENT_PATTERN}"); }
+    if !file_matches_format(&latest_week_file_path) {
+        println!("File content doesn't match the expected pattern {FILE_CONTENT_PATTERN}");
+        return;
+    }
 
     // create next week's plan from previous week's
+    let next_week_file_content = create_content_from_prev_week(latest_week_file_path);
+
+    // create next week's plan file pathname from previous week's
+    let next_week_file_path = create_path_from_prev_week(latest_week_file_path);
+
+    // write next week's plan content to the file
+    println!("Writing next week's plan into: {next_week_file_path}");
+    fs::write(next_week_file_path, next_week_file_content).expect("Couldn't write to file!");
+    println!("Done!");
+}
+
+fn find_latest_weekly_plan(dir_path: &str) -> Option<PathBuf> {
+    let regex = Regex::new(FILE_NAME_PATTERN).unwrap();
+
+    let latest_week_file_path = fs::read_dir(dir_path).ok()?
+        .filter(|f|
+            regex.is_match(&f.as_ref().unwrap().file_name().into_string().unwrap()))
+        .max_by(|x, y|
+            x.as_ref().unwrap().file_name().cmp(&y.as_ref().unwrap().file_name()))?.ok()?
+        .path();
+
+    Some(latest_week_file_path)
+}
+
+fn file_matches_format(path: &Path) -> bool {
+    let latest_week_file_content = fs::read_to_string(path).expect("Couldn't read previous week's file!");
+    let regex = Regex::new(FILE_CONTENT_PATTERN).unwrap();
+
+    regex.is_match(&latest_week_file_content)
+}
+
+fn create_content_from_prev_week(path: &Path) -> String {
     let latest_week_file = OpenOptions::new()
         .read(true)
         .write(true)
-        .open(latest_week_file_path)
-        .unwrap();
+        .open(path)
+        .expect("Couldn't open previous week's file");
+
     let regex = Regex::new(FILE_DELETION_PATTERN).unwrap();
-    let next_week_file_content = BufReader::new(latest_week_file)
+    BufReader::new(latest_week_file)
         .lines()
         .map(|x| x.unwrap())
         .filter(|l| !regex.is_match(&l))
         .collect::<Vec<String>>()
-        .join("\n");
+        .join("\n")
+}
 
-    // create next week's plan file name from previous week's
-    let next_week_file_path = latest_week_file_path.to_str().unwrap();
+fn create_path_from_prev_week(path: &Path) -> String {
+    let next_week_file_path = path.to_str().unwrap();
     let latest_week_date = next_week_file_path.split_at(next_week_file_path.len() - 11).1.split_at(8).0;
     let next_week_date = NaiveDate::parse_from_str(latest_week_date, "%Y%m%d")
         .unwrap()
         .checked_add_days(Days::new(7))
         .unwrap();
     let next_week_date = next_week_date.to_string().replace("-", "");
-    let next_week_file_path = next_week_file_path.split_at(next_week_file_path.len() - 11).0.to_owned() + &next_week_date.to_string() + ".md";
 
-    // write next week's plan content to the file
-    println!("Writing next week's plan into: {next_week_file_path}");
-    fs::write(next_week_file_path, next_week_file_content).expect("Couldn't write to file!");
-
-    println!("Done!");
-}
-
-fn find_latest_weekly_plan(dir_path: &str) -> PathBuf {
-    let regex = Regex::new(FILE_NAME_PATTERN).unwrap();
-
-    let latest_week_file_path = fs::read_dir(dir_path)
-        .expect(&format!("Couldn't read the directory {dir_path}"))
-        .filter(|f|
-            regex.is_match(&f.as_ref().unwrap().file_name().into_string().unwrap()))
-        .max_by(|x, y|
-            x.as_ref().unwrap().file_name().cmp(&y.as_ref().unwrap().file_name()))
-        .unwrap()
-        .unwrap()
-        .path();
-
-    latest_week_file_path
+    next_week_file_path.split_at(next_week_file_path.len() - 11).0.to_owned() + &next_week_date.to_string() + ".md"
 }
